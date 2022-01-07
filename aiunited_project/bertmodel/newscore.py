1e39603a1dc7baf67b8ad106a7d81d1fa2785ec9
juhyeon <quzmini@naver.com>
2022-01-08 02:33:51 +0900

    2022-01-08 modifying newscore

--- aiunited_project/bertmodel/newscore.py
+++ aiunited_project/bertmodel/newscore.py
@@ -14,22 +14,23 @@ def avg_score_to_csv(path):
 
     no_store_review_lst = []
     for store_id in tqdm(info_df['store_id']):
-        review_df = df[df['store_id'] == store_id]
+        try:
+            review_df = df[df['store_id'] == store_id]
 
-        if review_df.empty:
-            no_store_review_lst.append(store_id)
-            continue
+            if review_df.empty:
+                no_store_review_lst.append(store_id)
+                continue
 
-        review_cnt = len(review_df['cluster'])
-        if review_cnt == 0 :
-            pass
-        else:
+            review_cnt = len(review_df['cluster'])
             new_score = np.round(sum(review_df['cluster']) / review_cnt, 2)
             info_df.loc[(info_df['store_id'] == store_id), 'new_score'] = new_score
             info_df.loc[(info_df['store_id'] == store_id), 'review_cnt'] = review_cnt
+        except ZeroDivisionError:
+            pass
 
     for store_id in info_df['store_id']:
         if store_id in no_store_review_lst:
             info_df.loc[(info_df['store_id'] == store_id), 'new_score'] = ''
             info_df.loc[(info_df['store_id'] == store_id), 'review_cnt'] = 0
     info_df.to_csv('new_storeInfo.csv', encoding='utf-8', index=False)
+    print(info_df)
